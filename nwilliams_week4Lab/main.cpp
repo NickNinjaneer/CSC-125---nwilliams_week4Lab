ebe7910607e2d685664169173d2f121780c5e7f0
Nick Williams <SoundsCustom@gmail.com>
2015-02-26 21:00:19 -0600

    Initial Commit

---
+++ nwilliams_week4Lab/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  nwilliams_week4Lab
+//
+//  Created by Nick Williams on 2/26/15.
+//  Copyright (c) 2015 Ninjaneer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
